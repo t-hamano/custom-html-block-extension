e5f12ffc35c3af030edf70cbe5aff0cb939d16c6
Tetsuaki Hamano / 浜野 哲明 <tetsuaki.hamano@gmail.com>
2022-05-22 16:40:09 +0000

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+const config = {
+	...require( '@wordpress/prettier-config' ),
+	semi: true,
+	useTabs: true,
+	tabWidth: 2,
+	singleQuote: true,
+	printWidth: 100,
+	bracketSpacing: true,
+	parenSpacing: true,
+	// Set new property instead of jsxBracketSameLine
+	bracketSameLine: false,
+}
+
+// Remove deprecated property
+delete config.jsxBracketSameLine
+
+module.exports = config;
