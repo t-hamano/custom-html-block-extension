a99bc4316999f2f106ec006ac594c9a99e1d2f4b
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2024-03-08 13:35:55 +0900

    Remove `skip`

--- test/e2e/test.spec.js
+++ test/e2e/test.spec.js
@@ -4,7 +4,7 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 
 test.describe( 'Custom HTML Block Extension', () => {
-	test.describe.skip( 'Editor', () => {
+	test.describe( 'Editor', () => {
 		test( 'input by Emmet should be expanded on the classic editor', async ( {
 			admin,
 			page,
