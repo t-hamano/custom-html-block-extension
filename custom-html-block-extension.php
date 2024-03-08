876a1655c3c4a7201c38bb7aa641c1755ab7caf0
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2024-03-08 11:20:43 +0900

    Drop support for WP6.2 (#101)
    * Drop support for WP6.2
    
    * Fix e2e test

--- custom-html-block-extension.php
+++ custom-html-block-extension.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Custom HTML Block Extension
  * Description: Extend Custom HTML block to evolve into the advanced code editor.
- * Requires at least: 6.2
+ * Requires at least: 6.3
  * Requires PHP: 7.4
  * Version: 3.4.0
  * Author: Aki Hamano
