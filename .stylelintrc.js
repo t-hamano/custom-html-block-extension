1615602c02e05f2c9e1d1e9fb95319ad453bbf74
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-03-17 21:50:33 +0900

    Stylelint: Use Stylistic

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: [ '@wordpress/stylelint-config/scss' ],
+	extends: [ '@wordpress/stylelint-config/scss-stylistic' ],
 	ignoreFiles: [
 		'build/**/*.css',
 		'node_modules/**/*.css',
