366b5d7ec057d13c3f969b3bba369374447dcc27
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2025-11-08 22:35:31 +0900

    Update npm packages (#144)

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,12 @@ module.exports = {
 		modules: [ 'node_modules', 'src' ],
 		extensions: [ '.js', '.jsx' ],
 	},
+	output: {
+		...defaultConfig.output,
+		clean: false,
+	},
 	performance: {
 		maxEntrypointSize: 1000000,
 		maxAssetSize: 1000000,
 	},
-	plugins: [
-		// Don't delete files in the build directory
-		...defaultConfig.plugins.filter( ( plugin ) => {
-			return plugin.constructor.name !== 'CleanWebpackPlugin';
-		} ),
-	],
 };
