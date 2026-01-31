bb30b20609d7b9e281944475f264c9aee0b36533
Aki Hamano <tetsuaki.hamano@gmail.com>
2026-01-31 19:50:55 +0900

    Run e2e tests on Firefox and Safari

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+const { devices } = require( '@playwright/test' );
+
+/**
  * WordPress dependencies
  */
 const config = require( '@wordpress/scripts/config/playwright.config.js' );
@@ -6,4 +11,18 @@ const config = require( '@wordpress/scripts/config/playwright.config.js' );
 export default {
 	...config,
 	testDir: './test/e2e',
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices[ 'Desktop Chrome' ] },
+		},
+		{
+			name: 'firefox',
+			use: { ...devices[ 'Desktop Firefox' ] },
+		},
+		{
+			name: 'webkit',
+			use: { ...devices[ 'Desktop Safari' ] },
+		},
+	],
 };
